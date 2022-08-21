614cfac8e9dc135f4474ecca98be4868f9f264c5
najahmitchell <najahmitchell5@gmail.com>
2022-08-21 13:50:14 -0500

    first commit

---
+++ src/components/Navbar/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className='navbar'>
+    <h5>Nav</h5>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../../components/Navbar/Nav'
+
+const Home = () => {
+  return (
+    <div>
+      <Nav/>
+      <h1>NavTest</h1>
+    </div>
+    
+  )
+}
+
+export default Home
