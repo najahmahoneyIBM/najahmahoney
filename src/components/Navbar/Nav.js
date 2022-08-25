4ef4f636668dc28bf1a9c0f3736f78c546fa69c4
najahmitchell <najahmitchell5@gmail.com>
2022-08-24 20:22:27 -0500

    image added to home page

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
       <ul className="nav navbar-nav navbar-right">
         <li><a href="./Work">Work</a></li>
         <li><a href="./Skill">Skills</a></li>
-        <li><a href="./About">About</a></li>
+        <li><a href="./About">About me</a></li>
         <li><a href="./Contact">Contact</a></li>
       </ul>
     </div>
